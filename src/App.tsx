78c9b3c30b5b1c8d010e2d2131131c8af998929f
Nicolas Cafure <nicocafure@gmail.com>
2021-07-29 11:46:52 -0300

    Menu superior con Router y logo

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,41 @@
 import React from 'react';
 import logo from './logo.gif';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 
 function App() {
-
-
+  
   return (
-    <div className="App">
-     
-    </div>
+    <> <Router>
+     <div className="contenedor-nav"><div className="logo-top"><img src={logo} alt=""></img></div>
+         <nav>      
+         <Link to="/">HOME</Link>
+         <Link to="/catalogo">CATALOGO</Link>
+       </nav></div>
+
+
+        <Switch>
+          <Route path="/catalogo">
+            catalogo
+          </Route>
+
+          <Route path="/">
+           Home
+          </Route>
+        </Switch>
+    
+  </Router>
+
+
+
+
+
+    </>
   );
 }
 
