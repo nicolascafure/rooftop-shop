77fd1ebba1298fdddeca2f7f7f0665253a0a515d
Nico Cafure <nicocafure@gmail.com>
2021-08-03 15:43:11 -0300

    Creando componente Catalogo

---
+++ src/pages/Catalogo.tsx
@@ -0,0 +1,27 @@
+
+import { IProduct } from "../interfaces/iShopStore";
+export interface CatalogoProps {
+  product: IProduct;
+  key: number;
+}
+
+const Catalogo: React.FunctionComponent<CatalogoProps> = () => {
+
+
+
+
+return(
+
+<div className="container-catalogo">
+
+
+
+<h1>Catalogo de productos</h1>
+
+
+</div>
+
+
+
+)
+}
