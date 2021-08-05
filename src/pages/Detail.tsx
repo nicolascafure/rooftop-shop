eceec125f714c7cd31fc63f51d4bf43d5fb31bb0
Nicolas Cafure <nicocafure@gmail.com>
2021-08-05 11:39:17 -0300

    Mostrando los datos del producto en el detalle

--- src/pages/Detail.tsx
+++ src/pages/Detail.tsx
@@ -36,17 +36,40 @@ const transformarImg=(imagenes:Array<string>)=>{
   return images
 }
 
+const descuento=(product:IProduct) :number=>{
+  if(product.offer===null){
+    return 0}else{
+      const a = product.offer.price / Number(product.price) *100
+      return 100- a
+    }
+  }
+
 
 
     return (  <>
     {product !==undefined ? 
+    <div className="flex-center">
     <div className="container-detail">
-    <h1>{product.title}</h1>
     <div className="detail-img">
     <ImageGallery items={transformarImg(product.images)} />
     </div>
+      <div className="detail-data">
+    <h1>{product.title}</h1>
+    {product.offer === null ? (
+          <p className="price">{product.currency} {product.price}</p>
+     
+      ) : (<>
+          <p className="price-offer">{product.currency} {product.offer.price}</p>
+          <p className="last-price">{product.currency} {product.price}</p>
+          <p>{descuento(product)}% OFF</p>
+  </>
+      )}
+
+    </div>
+   
     
     
+    </div>
     </div>
     :null}
 
